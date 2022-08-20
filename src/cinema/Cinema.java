261c42817c9b11c7e373e792c4e4bba51af940bd
akovalevskaia <kovalevskaia.av@gmail.com>
2022-08-20 15:53:59 +0300

    Changes

--- src/cinema/Cinema.java
+++ src/cinema/Cinema.java
@@ -20,7 +20,7 @@ public class Cinema {
         }
     }
 
-      {
+      public  static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of rows:");
         int rows = sc.nextInt();
